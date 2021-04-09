366a93605d0b708f2135fa4e5514dd0d6e52b957
Zach-Morris <zach-morris@lambdastudents.com>
2021-04-09 13:01:13 -0400

    Header.js Complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,29 @@
 const Header = (title, date, temp) => {
+  //INSTANTIATE
+  const headerElement = document.createElement('div')
+  const dateElement = document.createElement('span')
+  const titleElement = document.createElement('h1')
+  const tempElement = document.createElement('span')
+  
+  dateElement.textContent = date
+  titleElement.textContent = title
+  tempElement.textContent = temp
+
+  //CSS CLASS / ATTRIBUTE
+  headerElement.classList.add('header')
+  dateElement.classList.add('date')
+  dateElement.classList.add('span')
+  titleElement.classList.add('h1')//CHECK
+  tempElement.classList.add('temp')
+  tempElement.classList.add('span')
+
+  //HIERARCHY
+  headerElement.appendChild(dateElement)
+  headerElement.appendChild(titleElement)
+  headerElement.appendChild(tempElement)
+
+  return headerElement
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -14,6 +39,11 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+  const myHeader = Header('My Name Is Project: *Appended*', 'April 9, 2021', '47*' )
+  const thisArea = document.querySelector('.header-container')
+
+  thisArea.append(myHeader)
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
